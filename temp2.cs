b58f434fc9e9207cbd62ed462e71050f6ae576d0
Sahith <sahithkalluri111@gmail.com>
2024-05-24 12:06:07 +0530

    update

---
+++ temp2.cs
@@ -0,0 +1,32 @@
+body {
+    margin: 0;
+    font-family: Arial, sans-serif;
+}
+
+.navbar {
+    background-color: #333;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 10px;
+}
+
+.navbar-left,
+.navbar-right {
+    flex: 1;
+}
+
+.navbar-middle {
+    flex: 2;
+    text-align: center;
+}
+
+.nav-link {
+    color: white;
+    text-decoration: none;
+    padding: 5px 10px;
+}
+
+.nav-link:hover {
+    background-color: #555;
+}
