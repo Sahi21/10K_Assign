274c461a4338eae03e7251d94c031c9e942f302f
Sahith <sahithkalluri111@gmail.com>
2024-06-18 17:13:19 +0530

    Bootstrap

---
+++ Javascript/pattern.js
@@ -0,0 +1,16 @@
+// for (let i = 1; i <= 5; i++) {
+//     let stars = '';
+//     for (let j = 1; j <= i; j++) {
+//       stars += '*';
+//     }
+//     console.log(stars);
+//   }
+  //
+
+  for(let i=1;i<=j;i++){
+    let stars = '';
+    for (let j=1;j<=10;j++){
+        stars += '*';
+    }
+    console.log(stars)
+  }
